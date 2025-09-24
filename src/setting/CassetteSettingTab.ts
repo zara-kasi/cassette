500539322b39d37eee174cd9499bb8f7e8b6d1b5
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-09-24 20:06:04 +0530

    Create CassetteSettingTab.ts

---
+++ src/setting/CassetteSettingTab.ts
@@ -0,0 +1,14 @@
+class CassetteSettingTab extends PluginSettingTab {
+	plugin: CassettePlugin;
+
+	constructor(app: App, plugin: CassettePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+	}
+}
